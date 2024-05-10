f7ec4fb555d66a4a2385763a5ffbfd2ef4414cb6
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2024-05-10 16:25:05 -0700

    Moved around files to reconfigure gitignore

--- ProjectW/Intermediate/Build/Win64/x64/ProjectWEditor/Development/Engine/SharedPCH.Engine.Cpp17.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "C:/Users/Kette/OneDrive/Documents/Unreal Projects/project-w/ProjectW/Intermediate/Build/Win64/x64/ProjectWEditor/Development/Engine/SharedDefinitions.Engine.Cpp17.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
--- ProjectW/Intermediate/Build/Win64/x64/ProjectWEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/Users/Kette/OneDrive/Documents/Unreal Projects/project-w/ProjectW/Intermediate/Build/Win64/x64/ProjectWEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
